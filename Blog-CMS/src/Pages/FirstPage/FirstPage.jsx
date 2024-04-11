ee49ab020610eede7f0fe9ab29adba0aac9199d4
dkanders12 <dkanders12@gmail.com>
2024-04-11 07:59:48 +0200

    nice

--- Blog-CMS/src/Pages/FirstPage/FirstPage.jsx
+++ Blog-CMS/src/Pages/FirstPage/FirstPage.jsx
@@ -1,7 +1,11 @@
+// FirstPage.jsx
+
 import React from "react";
 import { useGetQuery } from "../../Components/Fetching/Fetching";
 import { getAllBlogs } from "../../Components/Get/Get";
+
 import "./FirstPage.scss";
+
 const FirstPage = () => {
   const { data, isLoading, error } = useGetQuery(getAllBlogs, `games`);
 
@@ -16,18 +20,20 @@ const FirstPage = () => {
   return (
     <section className="main">
       <h1>Blog Posts</h1>
-      {data.blogs.map((blog) => (
-        <article id="Container" key={blog.id}>
-          <div className="sideLeft">
-            <h2>{blog.name}</h2>
-            <img src={blog.img.url} alt={blog.img.fileName} />
-          </div>
-          <div className="sideRight">
-            <p>{blog.tekst.text}</p>
-            <p>{blog.time}</p>
-          </div>
-        </article>
-      ))}
+      <div id="grid">
+        {data.blogs.map((blog) => (
+          <article id="Container" key={blog.id}>
+            <div className="sideLeft">
+              <h2>{blog.name}</h2>
+              <img src={blog.img.url} alt={blog.img.fileName} />
+            </div>
+            <div className="sideRight">
+              <p>{blog.tekst.text}</p>
+              <p>{new Date(blog.time).toLocaleString()}</p>
+            </div>
+          </article>
+        ))}
+      </div>
     </section>
   );
 };
